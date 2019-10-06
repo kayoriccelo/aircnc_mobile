2485b32a28f8f62ca5438b1c64edcf9fca27b0e1
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-10-06 19:06:54 -0300

    Created component List; Implemented app Spot;

--- src/apps/Spot/index.js
+++ src/apps/Spot/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Image, AsyncStorage, StyleSheet } from 'react-native';
+
+import logo from '../../../assets/logo.png';
+import { List } from '../../components';
+
 
 export default function Spot() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        });
+    }, []);
+
     return (
-        <View>
-            <Text>Spot</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Image
+                style={styles.logo}
+                source={logo}
+            />
+            <ScrollView>
+                {techs.map(tech => <List key={tech} tech={tech} url="/spots" />)}
+            </ScrollView>
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 20,
+    }
+})
