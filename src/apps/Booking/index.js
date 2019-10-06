59f76962aed70bbd156efdd583627a46fb11fbf4
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-10-06 19:29:07 -0300

    Implemented app Booking;

--- src/apps/Booking/index.js
+++ src/apps/Booking/index.js
@@ -1,10 +1,92 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, AsyncStorage, Text, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 
-export default function Booking() {
+import api from '../../services/api';
+
+
+export default function Booking({ navigation }) {
+    const id = navigation.getParam('id');
+    const [date, setDate] = useState('');
+
+    const handleSubmit = async () => {
+        const user_id = await AsyncStorage.getItem('user');
+
+        await api.post(`/spots/${id}/bookings`, { date }, {
+            headers: { user_id }
+        });
+
+        Alert.alert('Solicitação de reserva enviada.');
+
+        navigation.navigate('Spot');
+    };
+
+    const handleCancel = () => {
+        navigation.navigate('Spot');
+    };
+
     return (
-        <View>
-            <Text>Booking</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.label}>DATA DE INTERESSE *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Qual data você quer reservar?"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleSubmit}
+            >
+                <Text style={styles.buttonText}>Solicitar reseva</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={[styles.button, styles.cancelButton]}
+                onPress={handleCancel}
+            >
+                <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 30,
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+        marginTop: 30
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+    cancelButton: {
+        backgroundColor: '#ccc',
+        marginTop: 10
+    },
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
